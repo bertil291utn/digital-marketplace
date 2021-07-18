62eeffb823a6156a30120fdb965d700d363ff18a
Bertil T <bertilt4@gmail.com>
2021-07-17 22:08:39 -0500

    :zap: Add new contracts addresses - Update tailwind configuration

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "nft-contract-address"
-export const nftaddress = "market-contract-address"
+export const nftmarketaddress = "0xDdDc4170ea3bd48D494D1378A36E8B6d685876Fc"
+export const nftaddress = "0x53840c40B9B1430E5d729E2596350C925c5Be34c"
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 /* pages/_app.js */
 import Link from 'next/link'
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+// import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
