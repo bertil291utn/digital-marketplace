a59acf7aa5785f0dafe44dd72881d2093338ff3f
Bertil T <bertilt4@gmail.com>
2021-07-20 22:46:18 -0500

    remove from git cache

--- config.js
+++
@@ -1,5 +0,0 @@
-// export const nftmarketaddress = "0xDdDc4170ea3bd48D494D1378A36E8B6d685876Fc"
-// export const nftaddress = "0x53840c40B9B1430E5d729E2596350C925c5Be34c"
-// localhost network
-export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
