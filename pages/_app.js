9f4376986f6803f888b251b059283cfc52e1a1e9
Bertil T <bertilt4@gmail.com>
2021-08-09 10:27:10 -0500

    :art: underline active link

--- pages/_app.js
+++ pages/_app.js
@@ -10,22 +10,22 @@ function MyApp({ Component, pageProps }) {
         <p className="text-4xl font-bold">Punk guitars marketplace</p>
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-4 text-pink-500">
+            <a className="mr-4 text-pink-500 focus:underline focus:font-bold">
               Home
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-pink-500 focus:underline focus:font-bold">
               Sell Digital Asset
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-pink-500 focus:underline focus:font-bold">
               My Digital Assets
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-pink-500 focus:underline focus:font-bold">
               Creator Dashboard
             </a>
           </Link>
