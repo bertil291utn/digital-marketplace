0355d738b5bce1023bcb10235a13ad17395a43ee
Bertil Tandayamo <bertilt4@gmail.com>
2022-09-07 21:29:49 -0500

    deploy just nft token contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,7 +12,7 @@ async function main() {
 
   const NFT = await ethers.getContractFactory('NFT');
   // const nft = await NFT.deploy(nftMarket.address);
-  const nft = await NFT.deploy(process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS);
+  const nft = await NFT.deploy();
   await nft.deployed();
   console.log('nft deployed to:', nft.address);
 }
