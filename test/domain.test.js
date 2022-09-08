15ea42b981cea53a5e7c6a0583a3996f53c7d9ab
Bertil Tandayamo <bertilt4@gmail.com>
2022-09-07 21:29:56 -0500

    update file name

---
+++ test/domain.test.js
@@ -0,0 +1,43 @@
+/* test/sample-test.js */
+const { expect } = require('chai');
+// const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+let _nftContract;
+let _owner;
+let _user1;
+
+async function deployIPFixture() {
+  const [owner, addr1, addr2] = await ethers.getSigners();
+  const NFT = await ethers.getContractFactory('NFT');
+  const nftContract = await NFT.deploy();
+  await nftContract.deployed();
+  _nftContract = nftContract;
+  _owner = owner;
+  _user1 = addr1;
+}
+
+describe('NFTMarket', async function () {
+  before(async () => {
+    await deployIPFixture();
+  });
+  it('Should mint an NFT', async function () {
+    let tx = await _nftContract.createToken('Dcaina');
+    await tx.wait();
+    expect(await _nftContract.balanceOf(_owner.address)).to.be.eq(1);
+  });
+
+  it('can not set a record', async function () {
+    await expect(
+      _nftContract.connect(_user1).setRecord('RECORD1', 'Dcaina')
+    ).to.be.revertedWith('You don not own a minted NFT');
+  });
+
+  it('should set a record', async function () {
+    let tx = await _nftContract.connect(_user1).createToken('Dcaina');
+    await tx.wait();
+    tx = await _nftContract.connect(_user1).setRecord('RECORD1', 'Dcaina');
+    await tx.wait();
+    expect(await _nftContract.getRecord('Dcaina')).to.be.eq(
+      'RECORD1'
+    );
+  });
+});
